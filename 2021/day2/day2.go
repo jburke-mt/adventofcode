27a4c6ba5460e945f366cf1f92716acf0ed97395
Johan Burke <jburke@milestechnologies.com>
2021-12-02 10:25:51 -0500

    moved stuff into functions

--- 2021/day2/day2.go
+++ 2021/day2/day2.go
@@ -6,15 +6,7 @@ import (
 	"jburke.com/advent_util"
 )
 
-func main() {
-	const filename = "input.txt"
-
-	inputSlice, err := advent_util.ReadLines(filename)
-
-	if err != nil {
-		panic(err)
-	}
-
+func movement_part1(inputCommands []string) complex128 {
 	commands := map[string]complex128 {
 		"forward": complex(1, 0),
 		"down": complex(0, 1),
@@ -22,7 +14,7 @@ func main() {
 	};
 	// Part 1
 	location := complex(0, 0)
-	for _, line := range inputSlice {
+	for _, line := range inputCommands {
 		var direction string
 		var distance float64
 
@@ -33,15 +25,25 @@ func main() {
 		}
 		location += commands[direction] * complex(distance, 0)
 	}
+	return location
+}
+
+func output_location(location complex128) {
 	fmt.Printf("Location: %v\n", location)
 	fmt.Printf("Product: %f\n", real(location)*imag(location))
+}
 
-	// Part 2
-	// still use the same commands, real part increases horizontal by X units and depth by aim * X units, imaginary part increases aim by X units
-
-	location = complex(0, 0)
+// Part 2
+// still use the same commands, real part increases horizontal by X units and depth by aim * X units, imaginary part increases aim by X units
+func movement_part2(inputCommands []string) complex128 {
+	commands := map[string]complex128 {
+		"forward": complex(1, 0),
+		"down": complex(0, 1),
+		"up": complex(0, -1),
+	};
+	location := complex(0, 0)
 	aim := 0.0
-	for _, line := range inputSlice {
+	for _, line := range inputCommands {
 		var direction string
 		var distance float64
 
@@ -54,6 +56,21 @@ func main() {
 		movementDelta := real(commands[direction]) * distance // if command is down or up this will just be an increase of 0
 		location += complex(movementDelta, aim * movementDelta)
 	}
-	fmt.Printf("Location: %v\n", location)
-	fmt.Printf("Product: %f\n", real(location)*imag(location))
+	return location
+}
+
+func main() {
+	const filename = "input.txt"
+
+	inputSlice, err := advent_util.ReadLines(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	location := movement_part1(inputSlice)
+	output_location(location)
+
+	location = movement_part2(inputSlice)
+	output_location(location)	
 }
